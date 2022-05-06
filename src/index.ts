67ebdb463c30810a04f029cb3b12f5c6abf6dbcc
marcusluis <marcusluis.mail@gmail.com>
2022-05-06 15:43:03 -0300

    estruturando backend

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+// Ativando o express e cors
+import express, { Express } from "express";
+import cors from "cors";
+import { AddressInfo } from "net";
+import knex from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Criando servidor
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
+});
+
+// Estabelece a conexão com o banco de dados
+export const connection = knex({
+    client: "mysql",
+    connection: {
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+    },
+});
